15e3ea66f3a8afa5c0ce2af77b3d690e33cdad0a
Mahmudur <mahmudhera93@gmail.com>
2023-10-02 14:19:45 -0400

    backing up

--- src/estimators_using_linear_eqns/solver.py
+++ src/estimators_using_linear_eqns/solver.py
@@ -1,41 +1,61 @@
 import pandas as pd
 import numpy as np
+from matplotlib import pyplot as plt
 
-ps_filter = 0.1
-pd_filter = 0.1
-d_filter = 0.1
-L_filter = 1000000
+range_low = 20
+range_high = 51
 
 if __name__ == '__main__':
-    df = pd.read_csv('observations.csv', delimiter=' ')
+    df = pd.read_csv('observations_more_freq_nts', delimiter=' ')
 
-    df = df[ df['p_s']==ps_filter ]
-    df = df[ df['p_d']==pd_filter ]
-    df = df[ df['d']==d_filter ]
-    #df = df[  df['L']==L_filter ]
+    ps_est_data = {}
+    for f_A in [ 1.0*i/100 for i in range(range_low, range_high) ]:
+        ps_est_data[f_A] = {}
+        for f_C in [ 1.0*i/100 for i in range(range_low, range_high) ]:
+            ps_est_data[f_A][f_C] = []
 
-    p_s_list = df['p_s'].tolist()
-    p_d_list = df['p_d'].tolist()
-    d_list = df['d'].tolist()
-    k_list = df['k'].tolist()
-    L_list = df['L'].tolist()
-    L2_list = df['L2'].tolist()
-    K1_list = df['K1'].tolist()
-    K2_list = df['K2'].tolist()
-    E_S_list = df['E_S'].tolist()
-    E_D_list = df['E_D'].tolist()
-    E_I_list = df['E_I'].tolist()
-    f_A_list = df['f_A_orig'].tolist()
-    f_A_mu_list = df['f_A_mut'].tolist()
+    print(ps_est_data.keys())
 
-    est_vals = []
+    ps_true = 0.01
+    df = df[ df['p_s']==ps_true ]
+    df = df[ df['k']==21 ]
+    df = df[ df['p_d']==0.01 ]
+    df = df[ df['d']==0.01 ]
 
-    for p_s, p_d, d, k, L, L2, K1, K2, S, D, I, f_A, f_A_mut in list( zip(p_s_list, p_d_list, d_list, k_list, L_list, L2_list, K1_list, K2_list, E_S_list, E_D_list, E_I_list, f_A_list, f_A_mu_list) ):
+    for index, row in df.iterrows():
+        mut_freqs = [row['f_A_mut'], row['f_C_mut'], row['f_G_mut'], row['f_T_mut']]
+        orig_freqs = [row['f_A_orig'], row['f_C_orig'], row['f_G_orig'], row['f_T_orig']]
+
+        f_A = -1
+        f_A_mut = -1
+        for i in range(4):
+            if orig_freqs[i] > f_A:
+                f_A = orig_freqs[i]
+                f_A_mut = mut_freqs[i]
+
+        L = row['L']
+        L2 = row['L2']
+        D = row['E_D']
+        S = row['E_S']
+        f_A_true = row['freq_A']
+        f_C_true = row['freq_C']
         try:
             p_s_est = (f_A_mut - f_A + L/4.0 - L2/4.0) / ( (L - 4 * f_A) * (1.0/3.0 + D/(4.0 * S) ) )
-        except ZeroDivisionError:
+            ps_est_data[f_A_true][f_C_true].append(p_s_est)
+        except:
             p_s_est = None
-        print(f'Solution using linear stuff: p_s={p_s_est}, True p_s={p_s}')
-        est_vals.append(p_s_est)
 
-    print(np.mean(est_vals), np.var(est_vals), np.mean(est_vals)/p_s)
+    for f_A in [ 1.0*i/100 for i in range(range_low, range_high) ]:
+        pdf_filename = f'test_plots/plot_f_a_{f_A}.pdf'
+        Z1 = []
+        Z2 = []
+        Z3 = []
+        for f_C in [ 1.0*i/100 for i in range(range_low, range_high) ]:
+            Z1.append( np.mean(ps_est_data[f_A][f_C]) )
+            Z2.append( np.mean(ps_est_data[f_A][f_C]) + np.var(ps_est_data[f_A][f_C])**0.5 )
+            Z3.append( np.mean(ps_est_data[f_A][f_C]) - np.var(ps_est_data[f_A][f_C])**0.5 )
+        plt.plot([ 1.0*i/100 for i in range(range_low, range_high) ], Z1)
+        plt.plot([ 1.0*i/100 for i in range(range_low, range_high) ], Z2)
+        plt.plot([ 1.0*i/100 for i in range(range_low, range_high) ], Z3)
+        plt.savefig(pdf_filename)
+        plt.clf()
